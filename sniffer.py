08a58f0f27fe22f542db1099614b3f497b9835ed
arkos666 <arkos666@gmail.com>
2021-06-21 15:03:31 +0200

    Finish the relationship between MACs scanned with, 'IP', 'vendor' and 'hostname'

--- sniffer.py
+++ sniffer.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 from scapy.all import sniff, getmacbyip, ARP
 import json
+import socket
 
 
 ## Primero vamos a escanear la red durante 60 segundos
@@ -15,9 +16,8 @@ import json
 ## 4.- We're going to read MAC vendors from csv file to a dictionary
 ## 5.- We will create a csv with MAC;IP;VENDOR; hostname (if it has hostname)
 
-sec = 5 # Seconds to scan the network
+sec = 20 # Seconds to scan the network
 JsonFile = "MacVendors.json"
-
 
 prev_MACs = 0
 dict_scan = {}# This dict will give the IP from given MAC
@@ -32,33 +32,69 @@ def loading():
 # IP packet scan
 def ip_deploy(packet):    
   if not (getmacbyip(packet[0][1].src) is None):
-    dict_scan[getmacbyip(packet[0][1].src)] = packet[0][1].src
+    dict_scan[getmacbyip(packet[0][1].src).replace(":", "").upper()] = packet[0][1].src
     loading()
   if not (getmacbyip(packet[0][1].dst) is None):
-    dict_scan[getmacbyip(packet[0][1].dst)] = packet[0][1].dst
+    dict_scan[getmacbyip(packet[0][1].dst).replace(":", "").upper()] = packet[0][1].dst
     loading()
   return 
 
 # ARP packet scan
 def arp_deploy(pkt):
-  dict_scan[pkt[ARP].hwsrc] = pkt[ARP].psrc
+  dict_scan[(pkt[ARP].hwsrc).replace(":", "").upper()] = pkt[ARP].psrc
   loading()
   return
+  
+def search_mac(dict_vendor, dict_result):
+  # we're going to search for mac's results in vendors
+  for mac_vendor in dict_vendor:
+    for mac_scanned in dict_scan:
+     # the start may be the same between mac_vendor and mac scanned
+      if mac_scanned.startswith(mac_vendor):
+        dict_summary = {}
+        dict_summary ["VENDOR"] = dict_vendor[mac_vendor]
+        ip = dict_scan[mac_scanned]
+        dict_summary ["IP"] = ip
+        
+        # if we don't find a name, we will return the IP
+        try:
+          # gethostbyaddr return 3 names we check if one of them is a str
+          host = socket.gethostbyaddr(ip)
+          for hostname in host:
+            if isinstance(hostname, str):
+              dict_summary ["NAME"] = hostname
+          if not "NAME" in dict_summary:
+            dict_summary ["NAME"] = ip
+
+        
+        except :
+          dict_summary ["NAME"] = ip
+
+        dict_result [mac_scanned] = dict_summary
+  return dict_result
+    
   
 def read_json():
   f = open(JsonFile)
   data  = json.load(f)
-  
-  for i in data["vendor"]:
-    print(i)
-  
   f.close()
   
-  return 
+  dict_vendor= {}
+  
+  for vendor in data["vendor"]:
+    #print (data["vendor"][vendor])
+    for mac in data["vendor"][vendor]:
+      #print(mac)
+      dict_vendor[mac.upper()] = vendor
+  
+  #print(dict_vendor)
+  
+  return dict_vendor
 
 if __name__ == '__main__':
   
   print ("Scanning during " + str(sec) + " seconds")
+  
   
   # sniffing ARP traffic from SCAPPY
   sniff(filter="arp", prn=arp_deploy, timeout = (sec/2), store=0)
@@ -66,8 +102,15 @@ if __name__ == '__main__':
   # sniffing IP traffic from SCAPPY
   sniff(filter="ip", prn=ip_deploy, timeout = (sec/2))
   
-  # now we have all the MACs and IP's in a dictionary, we're going to relation between vendors
-  print(dict_scan)
+  ## now we have all the MACs and IP's in a dictionary, we're going to relation between vendors
+  # print(dict_scan)
+  
   
   # We're going to read JSON file which has the MAC Vendor that we want know
-  read_json()
+  dict_vendor = read_json()
+  dict_result = {}
+  dict_result = search_mac(dict_vendor, dict_result)
+  print(dict_result)
+
+  
+  
